4842b70b412ca53678e67b4a85f61c81204ecc35
tijana.pandevska <tijana.pandevska@scalefocus.com>
2023-09-07 09:41:54 +0200

    order management microservice

---
+++ src/main/java/com/ecommerce/ordermanagementmicroservice/OrderManagementMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.ordermanagementmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderManagementMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderManagementMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecommerce/ordermanagementmicroservice/OrderManagementMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.ordermanagementmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderManagementMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
